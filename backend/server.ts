65a8e42e3715afc8fdaac866bb07641d81e3f669
Abdu-Rauf <abdur3auf@gmail.com>
2026-09-22 11:38:41 +0530

    Add client create endpoint with Zod schema validation

--- backend/server.ts
+++ backend/server.ts
@@ -2,11 +2,13 @@ import "dotenv/config";
 import express from "express";
 import loginController from "./controllers/loginController";
 import registerController from "./controllers/registerController";
+import clientController from "./controllers/clientController";
 
 const app = express();
 app.use(express.json());
 
-app.post("/auth/login", loginController)
-app.post("/auth/register", registerController)
+app.post("/auth/login", loginController);
+app.post("/auth/register", registerController);
+app.post("/admin/create_client",clientController);
 
 app.listen(3000);
